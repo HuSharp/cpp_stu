00cfdd748ef056bde4f8f995f5603c9b274d6504
HuSharp <husharp@163.com>
2022-01-09 23:56:47 +0800

    [feature_STL] start STL_algorithm. and review iterator

--- cplusplus-things/basic/const.cpp
+++ cplusplus-things/basic/const.cpp
@@ -4,14 +4,17 @@
  * @Author: HuSharp
  * @Date: 2021-12-18 23:14:05
  * @LastEditors: HuSharp
- * @LastEditTime: 2021-12-18 23:16:22
+ * @LastEditTime: 2022-01-09 22:07:45
  * @@Email: 8211180515@csu.edu.cn
  */
 const int a = 100;
 
+#include <iostream>
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    const int a;
-
+    const char* a = "hello" "world";
+    cout << a << endl;
     return 0;
 }
